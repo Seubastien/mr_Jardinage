c4aac48f70e7aa0337026c2edbec2e319f765e06
seubastien <sebastien.bourr@gmail.com>
2024-07-06 18:19:55 +0200

    dashboard/fin responsive page plant

--- public/script/main.js
+++ public/script/main.js
@@ -1,19 +1,4 @@
-//script du menu burger//
-const burger = document.querySelector('#burger')
-const newMenu = document.querySelector('.newMenu')
-burger.addEventListener('click', ()=>{
-   burger.style.display = 'none'
-   newMenu.style.display = 'flex'
 
-   let background = document.createElement('div')
-   background.classList.add('backgroundDim')
-   document.querySelector('body').appendChild(background)
-   background.addEventListener('click', () => {
-       background.remove()
-       burger.style.display = 'flex'
-       newMenu.style.display = 'none'
-   })
-})
 
 
 
